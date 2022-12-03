f900f118ac5305572725061fa170f1b2daeaacb0
Jesus Lagares <jesus.lagares@novatalent.com>
2022-12-03 18:25:43 +0100

    add basic files

---
+++ main.py
@@ -0,0 +1,7 @@
+from solutions.DAY_1.max_elves_calories import max_elves_calories as today_challenge
+
+def main():
+    print("The result of the challenge is: "+str(today_challenge()))
+
+if __name__ == "__main__":
+    main()
